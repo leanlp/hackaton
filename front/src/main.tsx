b58c3073db75ce5f32c0a377c31ba1663ed7f793
leandro <0xleanlp@gmail.com>
2023-06-16 07:57:48 -0300

    test

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+// import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 // const client = new ApolloClient( {
 // 	uri: 'http://127.0.0.1:8080/graphql',
 // 	cache: new InMemoryCache(),
