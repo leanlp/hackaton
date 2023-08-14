2be8ad4821d69dfa9e4f4bbbd30f7175a2e6538f
AgustinaGF <agustinagomez385@gmail.com>
2023-08-13 22:05:27 -0300

    viewHouses

--- front/src/pages/RegisterHouse.tsx
+++ front/src/pages/RegisterHouse.tsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { Button, TextField, Box } from "@mui/material";
 
 function RegisterHouse() {
-  const [name, setName] = useState("");
+  const [location, setLocation] = useState("");
   const [description, setDescription] = useState("");
   const [price, setPrice] = useState("");
   const [category, setCategory] = useState("");
@@ -15,64 +15,86 @@ function RegisterHouse() {
       setFile(e.target.files[0]);
     }
   };
-  const handleSubmit = async (e: { preventDefault: () => void; }) => {
+  const handleSubmit = async (e: { preventDefault: () => void }) => {
     e.preventDefault();
-  
+
     const formData = new FormData();
-    formData.append('name', name);
-    formData.append('description', description);
-    formData.append('price', price);
-    formData.append('category', category);
-    formData.append('seller', seller);
-    formData.append('email', email);
+    formData.append("name", location);
+    formData.append("description", description);
+    formData.append("price", price);
+    formData.append("category", category);
+    formData.append("seller", seller);
+    formData.append("email", email);
     if (file) {
-      formData.append('image', file);
+      formData.append("image", file);
     }
-  
-    const response = await fetch('https://api.keywise.com.ar/products', {
-      method: 'POST',
+
+    console.log(formData);
+    // const response = await fetch(
+    //   "https://keywisebackend.onrender.com/products",
+    const response = await fetch("https://api.keywise.com.ar/products", {
+      method: "POST",
       body: formData,
     });
-    
-    console.log(response)
-  
+
+    console.log(response);
+
     const data = await response.json();
-  
+
     // Handle the response from the server
     console.log(data);
   };
 
   return (
-    <Box component="form" onSubmit={handleSubmit} display="flex" flexDirection="column" maxWidth="300px" margin="0 auto">
-      <TextField 
-  label="Descripción" 
-  value={description} 
-  onChange={e => setDescription(e.target.value)} 
-  margin="normal"
-/>
-<TextField 
-  label="Precio" 
-  value={price} 
-  onChange={e => setPrice(e.target.value)} 
-  margin="normal"
-/>
-<TextField 
-  label="Categoría" 
-  value={category} 
-  onChange={e => setCategory(e.target.value)} 
-  margin="normal"
-/>
-<TextField 
-  label="Propietario" 
-  value={seller} 
-  onChange={e => setSeller(e.target.value)} 
-  margin="normal"
-/>
-<input 
-  type="file" 
-  onChange={handleFileChange}
-/>
-      <Button type="submit" variant="contained" color="primary" style={{ marginTop: '20px' }}>
+    <Box
+      component="form"
+      onSubmit={handleSubmit}
+      display="flex"
+      flexDirection="column"
+      maxWidth="300px"
+      margin="0 auto">
+      <TextField
+        label="Localidad"
+        value={location}
+        onChange={(e) => setLocation(e.target.value)}
+        margin="normal"
+      />
+      <TextField
+        label="Descripción"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+        margin="normal"
+      />
+      <TextField
+        label="Precio"
+        value={price}
+        onChange={(e) => setPrice(e.target.value)}
+        margin="normal"
+      />
+      <TextField
+        label="Categoría"
+        value={category}
+        onChange={(e) => setCategory(e.target.value)}
+        margin="normal"
+      />
+      <TextField
+        label="Propietario"
+        value={seller}
+        onChange={(e) => setSeller(e.target.value)}
+        margin="normal"
+      />
+      <TextField
+        label="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        margin="normal"
+      />
+      <input type="file" onChange={handleFileChange} />
+      <Button
+        type="submit"
+        variant="contained"
+        color="primary"
+        style={{ marginTop: "20px" }}>
         Enviar
       </Button>
     </Box>
